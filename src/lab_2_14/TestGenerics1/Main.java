2e631c866eef45a1d40b92d5850e389c0a3df52f
Alexey Kapustnik <alexey3572@gmail.com>
2017-12-01 12:58:47 +0200

    Update Main.java

--- src/lab_2_14/TestGenerics1/Main.java
+++ src/lab_2_14/TestGenerics1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        //а указать тип данных, который будет в generic классе?
         MyTupe myTupe1 = new MyTupe("aaa", 12, 142);
 
         MyTupe myTupe = new MyTupe(10.10, "bbb", "ccc");
